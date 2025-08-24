50b40ec40e0795131ee63f21e7c8ceb298be9ab1
PC_Remko <remko_baur@yahoo.de>
2025-08-24 23:03:09 +0200

    quicksave

--- Python/FIles/FindDuplicatedImages.py
+++ Python/FIles/FindDuplicatedImages.py
@@ -10,7 +10,7 @@ imageExtensions = ['.png','.PNG','.jpg','.mp4','.JPG','.MOV']
 
 config ={
    'sameSizeReq':True ,
-   'List_SingleFiles':True ,
+   'List_SingleFiles':False ,
    'debugPrint':False,
    'keep_subFolders':[],
    'keep_subFolders':[],
@@ -158,7 +158,13 @@ def findDuplicateList_Dict(item_list,_config = config):
         if len(sameFileDictList)==0:
             continue      
         else:
-            resultList.append(sameFileDictList)
+            if not _config['List_SingleFiles']:
+                if any([f['Delete']=='x' for f in sameFileDictList]):
+                    sameFileDictList2 =  [f for f in sameFileDictList if not 'SingleFile' in f['Tag']]   
+                    if len(sameFileDictList2)>0:                    
+                        resultList.append(sameFileDictList2)
+            else:
+                resultList.append(sameFileDictList)
              
     if len(search_list)>0 and _config['List_SingleFiles']:
         sameFileDictList =[]        
@@ -167,6 +173,8 @@ def findDuplicateList_Dict(item_list,_config = config):
             dummy['Tag'] = 'notProcessed'        
             sameFileDictList.append(dummy)
         resultList.append(sameFileDictList)
+    
+        
     return resultList
    
 def xlsExport_ValidatedDuplicateList(AnalysisResults,OutputFile):      
@@ -195,6 +203,7 @@ def delete_DuplicatedFiles(AnalysisResults,deleteFiles):
                     os.remove(k['FullPath'])
                 del_count+=1
     print(f"{del_count} images deleted")
+    return del_count
 
 def check_config(_config):    
     if _config['EnableDeleteFiles']:
@@ -277,6 +286,7 @@ def process_forEachSubfolder_secondRootList(root =r'\\IR_MedServ\photo\Events', 
     dirname = Path(root)
     subfolders = [f.name for f in dirname.iterdir() if f.is_dir()]
     subfolders.sort()
+    delcount = 0
     for sf in subfolders:
         if sf in _config['skip_subFolders']:
             continue        
@@ -285,25 +295,28 @@ def process_forEachSubfolder_secondRootList(root =r'\\IR_MedServ\photo\Events', 
         for secFolder in secondFolderList:
             item_list += get_ImageList_for_folderTree(rootdir_glob = secFolder)
         AnalysisResults=findDuplicateList_Dict(item_list,_config)
-        xlsExport_ValidatedDuplicateList(AnalysisResults,f'{fileName}.xlsx')
-        delete_DuplicatedFiles(AnalysisResults,deleteFiles=_config['EnableDeleteFiles']) 
-        
+        if len(AnalysisResults)>0:
+            xlsExport_ValidatedDuplicateList(AnalysisResults,f'{fileName}.xlsx')
+            delcount += delete_DuplicatedFiles(AnalysisResults,deleteFiles=_config['EnableDeleteFiles']) 
+    print("=======================================") 
+    print(f"Total deleted files = {delcount}") 
 # testing1()
 # testing2()
 # testing3()
 
 _config = copy.deepcopy(config)
-_config['remove_subFolders'] =['Afterworld','Dias','raw','lowres']
+_config['remove_subFolders'] =['Afterworld','Dias','raw','lowres','usschuss']
 _config['keep_subFolders'] =['Best','best','uswahl']
-_config['skip_subFolders'] =['0_Remko','0_Ira']
-_config['sameSizeReq'] = False
-_config['EnableDeleteFiles'] = False   
+_config['skip_subFolders'] =['0_Remko','0_Ira'] # 
+_config['sameSizeReq'] = True
+_config['EnableDeleteFiles'] = True   
 _config['DeleteCopyInOtherPath'] = True   
 
 # process()
-process_folder(r'\\IR_MedServ\photo\Events\2024_06_13_Promotion_Verteidigung',_config = _config)
+# process_folder(r'\\IR_MedServ\photo\Events\2024_06_13_Promotion_Verteidigung',_config = _config)
+process_folder(r'\\IR_MedServ\photo\Familie\Baur_LuM',_config = _config)
 # process_folder(r'\\IR_MedServ\Backup\Remko\_TestData',_config = _config)
 # process_folder(r'C:\GIT\_TestData',_config = _config)
 # process_forEachSubfolder(r'\\IR_MedServ\photo\DCIM-Handy\Remko',_config = _config)
 # process_forEachSubfolder_secondRootList(r'\\IR_MedServ\photo\Events',[r'\\IR_MedServ\photo\share'],_config = _config)
-# process_forEachSubfolder_secondRootList(r'\\IR_MedServ\photo\Events',[],_config = _config)
+# process_forEachSubfolder_secondRootList(r'\\IR_MedServ\photo\Familie\Baur_LuM\Urlaubsfotos',[],_config = _config)
